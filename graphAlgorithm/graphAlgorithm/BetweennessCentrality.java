e8a792b6bf14dd6800b68767ce766adc6daf7947
Guisanpea <guisanpea@gmail.com>
2017-11-24 20:24:03 +0100

    Added pdf with annotations

--- graphAlgorithm/graphAlgorithm/BetweennessCentrality.java
+++ graphAlgorithm/graphAlgorithm/BetweennessCentrality.java
@@ -19,13 +19,14 @@ public class BetweennessCentrality implements NetworkCentrality {
 		int vertices = g.numVertices();
 
 		double[] betweenness = new double[vertices];
-		for (int k = 0; k < vertices; k++) {
-			for (int i = 0; i < vertices; i++) {
-				for (int j = 0; j < vertices; j++) {
-					if (g.get(i,j) == k) {
-						betweenness[k]++;
+		for (int i = 0; i < vertices; i++) {
+			for (int j = 0; j < vertices; j++) {
+				for (int k = 0; k < vertices; k++) {
+					if (g.get(j,k) == i) {
+						betweenness[i]++;
 					}
 				}
+				betweenness[i] /= vertices*vertices;
 			}
 		}
 		
