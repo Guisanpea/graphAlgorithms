ea4e8ad55443907db7dd9d374120ad3b3320a831
Guisanpea <guisanpea@gmail.com>
2017-11-25 19:29:25 +0100

    Corrrected mistake on betweenness centrality measure

--- graphAlgorithm/graphAlgorithm/BetweennessCentrality.java
+++ graphAlgorithm/graphAlgorithm/BetweennessCentrality.java
@@ -22,15 +22,21 @@ public class BetweennessCentrality implements NetworkCentrality {
 		for (int i = 0; i < vertices; i++) {
 			for (int j = 0; j < vertices; j++) {
 				for (int k = 0; k < vertices; k++) {
-					if (g.get(j,k) == i) {
+					if (goesTroughI(g,i,j,k))
 						betweenness[i]++;
-					}
 				}
 				betweenness[i] /= vertices*vertices;
 			}
 		}
 		
 		return betweenness;
+	}
+
+	private boolean goesTroughI(CGraph g, int i, int j, int k) throws Exception{
+        int vertex = g.get(j, k);
+		boolean goes = (vertex != k) &&
+                       ((vertex == i) || (goesTroughI(g,i,j,vertex)));
+	    return goes;
 	}
 
 	/*
